30378bf8e7aca7850632d371f6e19b8d04c85d39
Sydney <proteimus@gmail.com>
2024-02-03 12:18:20 -0600

    setCount: increment/decrement

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
-  return <h2>Hello, World!</h2>
+  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState('')
+
+  const increment = evt => {
+    setCount(count + 1)
+  }
+  const decrement = evt => {
+    setCount(count - 1)
+  }
+  const changeInput = evt => {
+    const { value } = evt.target
+    setInputValue(value)
+  }
+  return (
+    <div>
+      <div>the count is {count}</div>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <input value={inputValue} onChange={changeInput} />
+      <div>{inputValue.length ? 'You typed' : ''} {inputValue}</div>
+      <div>{inputValue.length} characters</div>
+    </div>
+  )
 }
 
 export default App
